c9fed9f50e0942545ea26bfb54ecfe4bdd1b902f
Justin Beaulieu <beaulieudev@gmail.com>
2018-12-31 01:28:21 +0000

    initial commit

---
+++ lib/ma_breweries.rb
@@ -0,0 +1,6 @@
+require "ma_breweries/version"
+
+module MaBreweries
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ma_breweries/version.rb
@@ -0,0 +1,3 @@
+module MaBreweries
+  VERSION = "0.1.0"
+end
