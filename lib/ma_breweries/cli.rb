efee73e079c4d9a5632e93fd6be70ed475aec3ba
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-06 20:32:11 +0000

    wrote search methods in cli.rb

--- lib/ma_breweries/cli.rb
+++ lib/ma_breweries/cli.rb
@@ -6,7 +6,6 @@ class MaBreweries::CLI
     puts "I can help you search but you will have to share!"
     menu
     brew_input = gets.chomp.downcase
-
     while (brew_input != exit)
       case brew_input
       when "name"
@@ -23,24 +22,60 @@ class MaBreweries::CLI
       else
         puts "I am sorry you won't find any brews that way!"
       end
-
       menu
-
       brew_input = gets.chomp.downcase
     end
     puts "Peace out! Have fun and drive carefully!"
   end
 
   def menu
-      puts "There are many ways fo you to search."
-      puts "1. To search by brewery name please enter - Name"
-      puts "2. To search by brewery type please enter - Type"
-      puts "3. To search by street name please enter - Street"
-      puts "4. To search by city please enter - City"
-      puts "5. To list all the breweries by name please enter - Let the beer flow"
-      puts "6. to exit please enter - I had to much"
-    end
-
+    puts "There are many ways fo you to search."
+    puts "1. To search by brewery name please enter - Name"
+    puts "2. To search by brewery type please enter - Type"
+    puts "3. To search by street name please enter - Street"
+    puts "4. To search by city please enter - City"
+    puts "5. To list all the breweries by name please enter - Let the beer flow"
+    puts "6. to exit please enter - I had to much"
   end
+
+  def search_breweries_by_name
+    puts "Please enter the brewery name: "
+    input = gets.chomp.downcase
+    brewery = MaBreweries::BREWERY.find_by_name(input)
+    if brewery
+      brewery.brew_info
+    end
+  end
+
+  def search_breweries_by_type
+    puts "Please enter the brewery type: "
+    input = gets.chomp.downcase
+    brewery = MaBreweries::BREWERY.find_by_brewery_type(input)
+    if brewery
+      brewery.brew_info
+    end
+  end
+
+  def search_breweries_by_street
+    puts "Pease enter the street you think the brews are on:"
+    input = gets.chomp.downcase
+    brewery = MaBreweries::BREWERY.find_by_street(input)
+    if brewery
+      brewery.brew_info
+    end
+  end
+
+  def search_breweries_by_city
+    puts "Please enter the city you would like to search in:"
+    input = gets.chomp.downcase
+    brewery = MaBreweries::BREWERY.find_by_city(input)
+    if brewery
+      brewery.brew_info
+    end
+  end
+
+  def show_all_breweries
+  end
+
 
 end
