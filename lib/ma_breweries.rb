80d7262f46df526d728170856eda229aebbbcab5
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-08 00:30:42 +0000

    added the ability to return search without staring the

--- lib/ma_breweries.rb
+++ lib/ma_breweries.rb
@@ -4,7 +4,7 @@ require "json"
 require_relative "./ma_breweries/version"
 require_relative "./ma_breweries/brewery"
 require_relative "./ma_breweries/cli"
-require_relative "./ma_breweries/api"
+
 
 module MaBreweries
   class Error < StandardError; end
--- lib/ma_breweries/brewery.rb
+++ lib/ma_breweries/brewery.rb
@@ -16,15 +16,6 @@ class MaBreweries::BREWERY
       @@all
   end
 
-
-  # def self.create_from_api
-  #   api = MaBreweries::API.new
-  #   results = breweries.collect do |attributes_hash|
-  #    self.new(attributes_hash)
-  #   end
-  #   @all << self
-  #  end
-
   def self.all_names
     self.all.each_with_index do |brewery, i|
     puts "#{i+1}: #{brewery.name}"
@@ -32,20 +23,20 @@ class MaBreweries::BREWERY
   end
 
   def self.find_by_name(name)
-    self.all.detect {|brewery| brewery.name == name}
+    self.all.detect {|brewery| brewery.name.downcase == name.downcase}
 
   end
 
   def self.find_by_brewery_type(brewery_type)
-    self.all.select {|brewery| brewery.brewery_type == brewery_type}
+    self.all.select {|brewery| brewery.brewery_type.downcase == brewery_type.downcase}
   end
 
   def self.find_by_street(street)
-    self.all.select {|brewery| brewery.street == street}
+    self.all.select {|brewery| brewery.street.downcase == street.downcase}
   end
 
   def self.find_by_city(city)
-    self.all.select {|brewery| brewery.city == city}
+    self.all.select {|brewery| brewery.city.downcase == city.downcase}
   end
 
   def self.all_names
